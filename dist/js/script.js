bebd9d91ceaea7e8d20c8d98888a4d857c82a8ce
VVasilevich <vve913@gmail.com>
2023-05-15 21:15:56 +0600

    Add cutNumbers

--- dist/js/script.js
+++ dist/js/script.js
@@ -380,11 +380,14 @@ window.addEventListener('DOMContentLoaded', () => {
     dotsArr.forEach(dot => dot.style.opacity = '.5');
     dotsArr[slideIndex - 1].style.opacity = 1;
   }
+  function cutNumbers(str) {
+    return +str.replace(/\D/g, '');
+  }
   next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == cutNumbers(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += cutNumbers(width);
     }
     if (slideIndex == slides.length) {
       slideIndex = 1;
@@ -395,9 +398,9 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   prev.addEventListener('click', () => {
     if (offset == 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = cutNumbers(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= cutNumbers(width);
     }
     if (slideIndex == 1) {
       slideIndex = slides.length;
@@ -410,7 +413,7 @@ window.addEventListener('DOMContentLoaded', () => {
     dot.addEventListener('click', e => {
       const slideTo = e.target.getAttribute('data-slide-to');
       slideIndex = slideTo;
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = cutNumbers(width) * (slideTo - 1);
       updateSlides();
     });
   });
--- src/js/main.js
+++ src/js/main.js
@@ -327,11 +327,15 @@ window.addEventListener('DOMContentLoaded', () => {
         dotsArr[slideIndex - 1].style.opacity = 1;
     }
 
+    function cutNumbers(str) {
+        return +str.replace(/\D/g, '')
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length -2) * (slides.length - 1)) {
+        if (offset == cutNumbers(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length -2);
+            offset += cutNumbers(width);
         }
 
         if (slideIndex == slides.length) {
@@ -344,9 +348,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length -2) * (slides.length - 1);
+            offset = cutNumbers(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length -2);
+            offset -= cutNumbers(width);
         }
 
         if (slideIndex == 1) {
@@ -361,7 +365,7 @@ window.addEventListener('DOMContentLoaded', () => {
         dot.addEventListener('click', (e) => {
             const slideTo = e.target.getAttribute('data-slide-to');
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length -2) * (slideTo - 1);
+            offset = cutNumbers(width) * (slideTo - 1);
             updateSlides()
         });
     });
